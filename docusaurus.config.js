c1aaeb34ac6aeff88275ef0218288a357aaef0ab
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-10-05 14:00:16 +0900

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,10 +32,14 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: false,
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
         blog: {
-          routeBasePath: '/', // Serve the blog at the site's root
-          /* other blog options */
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
