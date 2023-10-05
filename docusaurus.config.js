1301b2ec244434e9d0724579e9b05e4d4d529d9a
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-10-05 13:58:13 +0900

    disable docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,14 +32,10 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        docs: false,
         blog: {
+          routeBasePath: '/', // Serve the blog at the site's root
+          /* other blog options */
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
