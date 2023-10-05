458c1337665b6b1fcae901948f1d57c24146bbe8
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-10-05 14:13:02 +0900

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'K's Tech Blog,
+        title: 'Ks Tech Blog',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
